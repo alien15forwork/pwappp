08395def50b31e2f039597abf4d6f8d58f71b0ab
alien15forwork <134682185+alien15forwork@users.noreply.github.com>
2024-02-21 22:03:05 -0500

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,34 @@
+
+// 下载按钮
+let installBtn = document.querySelector('.installApp')
+ 
+// 下载对象
+let appPromptEvent = null
+ 
+// 浏览器地址支持下载的话触发此事件
+window.addEventListener('beforeinstallprompt', e => {
+    appPromptEvent = e
+})
+ 
+// 点击下载按钮
+installBtn.addEventListener('click', () => {
+    if (appPromptEvent !== null) {
+        // 调用安装
+        appPromptEvent.prompt()
+        // 回调
+        appPromptEvent.userChoice.then((res)=>{
+            if (res.outcome === 'accepted') {
+                console.log('install succeed')
+            }
+            else{
+                console.log('cancel install')
+            }
+            appPromptEvent = null
+        })
+    }
+})
+ 
+// 监听安装完成
+window.addEventListener('appinstalled',() => {})
+ 
+// 判断是否桌面打开
